6b83d64f2be4ab010eb50eab5b34c013e8e6ce68
paulngouchet <ngouchet@bu.edu>
2020-08-02 13:14:57 -0400

    Clean Code

--- Homework#4/Couples.cpp
+++ Homework#4/Couples.cpp
@@ -1,64 +1,41 @@
 #include "Couples.h"
 
+Couples::Couples(Coordinates* point1, Coordinates* point2){
+	this->point1 = point1;
+	this->point2 = point2;
+}
+double Couples::slope(){
+	double slp = 0 ;
+	slp = (point1->y - point2->y)/(point1->x - point2->x);
+	return slp;
+}
 
-	Couples::Couples(Coordinates* point1, Coordinates* point2)
-	{
-		this->point1 = point1;
-		this->point2 = point2;
+void Couples::print(){
+	cout << "(" << point1->x << "," << point1->y << ")" << endl;
+	cout << "(" << point2->x << "," << point2->y << ")" << endl;
+}
 
+bool Couples::equationTest( Coordinates* point){
+	double slope1 = slope();
+	double intercept = 0.0 ;
+	
+	if(isinf(slope1) == 1 ){
+		if(point1->x == point->x)
+			return true;
 	}
-
-	double Couples::slope()
-	{
-		double slp = 0 ;
-
-		slp = (point1->y - point2->y)/(point1->x - point2->x);
-		return slp;
-
-
+	else if(slope1 == 0.0){
+		if(point1->y == point->y)
+			return true;
 	}
-
-	void Couples::print()
-	{
-
-		cout << "(" << point1->x << "," << point1->y << ")" << endl;
-		cout << "(" << point2->x << "," << point2->y << ")" << endl;
+	else {
+		intercept = point1->y - (slope1 * point1->x);
+		double result = point->y - (slope1*point->x) - intercept;
+		if( result == 0.000000)
+			return true ;
 	}
-
-	bool Couples::equationTest( Coordinates* point)
-	{
-		double slope1 = slope();
-
-		double intercept = 0.0 ;
-
-		if(isinf(slope1) == 1 )
-		{
-			if(point1->x == point->x)
-				return true;
-		}
-		else if( slope1 == 0.0)
-		{
-			if(point1->y == point->y)
-				return true;
-		}
-		else 
-		{
-			
-
-			intercept = point1->y - (slope1 * point1->x);
-
-			double result = point->y - (slope1*point->x) - intercept;
-
-			if( result == 0.000000)
-				return true ;
-		}
-
-		return false ;
-
-
-
-
-	}
+	
+	return false ;
+}
 
 
 
