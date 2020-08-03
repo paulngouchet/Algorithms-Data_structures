cd6894096c22ed6d784e95e7a972079a443e06e6
paulngouchet <ngouchet@bu.edu>
2020-08-02 22:15:33 -0400

    Clean Code

--- Homework#8/BST.cpp
+++ Homework#8/BST.cpp
@@ -5,14 +5,11 @@
 #include "BST.h"
 
 /* constructs an empty Binary Search Tree */
-BST::BST()
-{
+BST::BST(){
     root = nullptr;
-}
-    
+}   
 /* constructs a one-node Binary Search Tree with the given key */
-BST::BST(int num)
-{
+BST::BST(int num) {
     root = new Node;
     root->key = num;
     root->left = nullptr;
@@ -21,30 +18,25 @@ BST::BST(int num)
     
 /* constructs a Binary Search Tree by repeatedly inserting ints in <nums>
     in the order they appear in the array */
-BST::BST(vector<int>& nums)
-{
+BST::BST(vector<int>& nums) {
     root = new Node;
     root->key = nums[0];
     root->left = nullptr;
     root->right = nullptr;
+    
     for (int i=1; i<nums.size(); i++)
         insert(nums[i]);
 }
     
 /* insert num as a new key to the BST */
-void BST::insert(int num)
-{
+void BST::insert(int num){
     insert(num, root);
 }
 
-
 /* insert num as a new key to the BST at node 'leaf' */
-void BST::insert(int num, Node *leaf)
-{
-    if (num > leaf->key) // insert to the right
-    {
-        if (leaf->right == nullptr)
-        {
+void BST::insert(int num, Node *leaf) {
+    if(num > leaf->key) {
+        if (leaf->right == nullptr){
             leaf->right = new Node;
             leaf->right->key = num;
             leaf->right->left = nullptr;
@@ -53,10 +45,8 @@ void BST::insert(int num, Node *leaf)
         else
             insert(num, leaf->right);
     }
-    else   // insert to the left
-    {
-        if (leaf->left == nullptr)
-        {
+    else {
+        if (leaf->left == nullptr){
             leaf->left = new Node;
             leaf->left->key = num;
             leaf->left->left = nullptr;
@@ -68,44 +58,39 @@ void BST::insert(int num, Node *leaf)
 }
 
 /* return the BST rooted at this node as a human-readable string */
-string BST::print()
-{
+string BST::print() {
     return print(root, 0);
 }
     
 /* rturn the BST rooted at leaf as a human-readable string,
     indented by <depth> characters */
-string BST::print(Node *leaf, int depth)
-{
+string BST::print(Node *leaf, int depth) {
     string result = "";
-    
     result.append(to_string(leaf->key)); // output the key
     result.append("\n");
         
-    if (leaf->left != nullptr)
-    {
+    if(leaf->left != nullptr) {
         result.append(RepeatChar(depth));
         result.append("L");
         result.append(print(leaf->left, depth+1));
     }
-    if (leaf->right != nullptr)
-    {
+    
+    if (leaf->right != nullptr){
         result.append(RepeatChar(depth));
         result.append("R");
         result.append(print(leaf->right, depth+1));
     }
-        
+    
     return result;
 }
     
 /* return a string of <depth> spaces */
-string BST::RepeatChar(int depth)
-{
+string BST::RepeatChar(int depth){
     string result = "";
         
     for (int i=0; i<depth; i++)
         result.append("-");
-        
+    
     return result;
 }
 
@@ -113,19 +98,16 @@ string BST::RepeatChar(int depth)
 /**
  * Represents one rotation (ZIG or ZAG) around one key in a Binary Search Tree
  */
-Rotation::Rotation(int num, RotationType rot)
-{
+Rotation::Rotation(int num, RotationType rot) {
     myKey = num;
     myRot = rot;
 }
 
 /* return A human-readable description of the rotation */
-string Rotation::print()
-{
+string Rotation::print() {
     string result="";
     
-    switch (myRot)
-    {
+    switch (myRot) {
         case ZIG:
             result="ZIG";
             break;
@@ -133,9 +115,9 @@ string Rotation::print()
             result="ZAG";
             break;
     }
+    
     result.append(" on ");
     result.append(to_string(myKey));
-    
     return result;
 }
 
